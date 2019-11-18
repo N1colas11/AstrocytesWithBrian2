ff0bf99cccf2b3fb735d82dcd6f7bbcfaa522df7
erlebach <gordon.erlebach@gmail.com>
2019-11-17 22:35:57 -0500

    Except for J_delta, the two codes GE and MDP are almost consistent.
    Diffusion is still not implemented in my code.
    Once Diffusion is done, next steps are to implement the branching tiers.
    Assume "n tiers", which are binary, symmetric.

--- Code/gordon/two_compartments_GE_2019-11-16.py
+++ Code/gordon/two_compartments_GE_2019-11-16.py
@@ -22,6 +22,7 @@ rate_ = lambda x,L,r : x*L*(1-r)
 ################################################################################
 ### General parameters
 duration = 60*second       # Total simulation time
+duration = 150*ms    # Total simulation time
 sim_dt = 50*ms               # Integrator/sampling step
 
 ### Astrocyte parameters
@@ -38,16 +39,18 @@ Omega_C = 6/second
 Omega_L = 0.1/second
 
 # Ratio of ER to cytosolic volume (assumed constant across cells)
-rho_A   = 0.18  # revisit
+rho_A   = 0.18  # revisit  Vol(ER) / Vol(cytosol)
+rho = rho_A / (1 + rho_A)
 
 # Values for r_c and r_l to use to get results consistent with MDP
+# MDP assumes a closed system. has equations for CaER
 r_c = Omega_C * rho_A
 r_l = Omega_L * rho_A
 
 # Value provided by MDP, taking into account that his equations are expressed in terms of C_T
 # there is no equation for C_ER (no need if diffusion of CaCYT and C_ER are zero)
-r_c = v_IPR = Omega_C = 6/second
-r_l = v_l = 0.11/second
+#r_c = v_IPR = Omega_C = 6/second
+#r_l = v_l = 0.11/second
 v_er = 1.0*umolar/second   # SERCA pump max amplitude
 
 k_er = 0.05*umolar
@@ -61,7 +64,7 @@ d_5 = 0.08234*umolar            # Ca^2+ activation dissociation constant
 k_ER = 0.05*umolar
 k_d = 0.7*umolar
 kappa_delta = 1*umolar
-K_delta = 0.5*umolar
+K_delta = 0.5*umolar    #<<<<<<<<<<<<<<<<<<
 k_pi = 0.6*umolar
 k_3 = k_3K = 1.*umolar
 k_R = 1.3 # units unclear
@@ -73,8 +76,9 @@ O_delta = 0.025*umolar/second
 O_3K = 2.*umolar/second
 O_5P = 0.05/second
 O_P = 1.*umolar/second   # J_SERCA (pump)
-Omega_L = 1./second   # Leak frequency
-Omega_C = 1./second   # IPR current frequency
+#Omega_L = 1./second   # Leak frequency
+#Omega_C = 1./second   # IPR current frequency
+print("***** 1-rho= ", 1-rho)
 Omega_2 = a_2 = 0.2/second/umolar     # Wrong units in Evan (2019) and Maurizio (2009) and perhaps Li and Rinzel, 
 O_2 = 1.*umolar/second # 
 
@@ -97,12 +101,19 @@ I_Theta = 0.3*umolar         # Threshold gradient for IP_3 diffusion
 # Additional and Modified Parameters
 ################################################################################
 Lambda = 2100*umeter**3
-# Different values than Evan in his thesis
-O_delta = 0.6  * umolar * Lambda * (1-rho_A) / second  # DERIVED HOW?
-O_3K    = 4.5  * umolar * Lambda * (1-rho_A) / second
-O_5P    = 0.05 * umolar * Lambda * (1-rho_A) / second
+# Different values than Evan in his thesis!!! Where does 0.6, 4.5, 0.0 come from? 
+# Values in Evan dissertation: O_delta= 0.025, O3k =2, r5p_bar=0.05
+O_delta = 0.6  * umolar * Lambda * (1-rho) / second  # DERIVED HOW?
+O_3K    = 4.5  * umolar * Lambda * (1-rho) / second
+O_5P    = 0.05 * umolar * Lambda * (1-rho) / second
 F       = 0.1  * Hz   # Maximal exogeneous IP3 flow
 D       = 0.05 / second
+
+print("******* r_l= ", r_l)
+print("******* rho_A= ", rho_A)
+print("******* Omega_L= ", Omega_L)
+print("******* O_delta= ", O_delta)
+print("******* K_delta= ", K_delta)
 
 ################################################################################
 # Model definition
@@ -113,32 +124,40 @@ defaultclock.dt = sim_dt     # Set the integration time
 # Alternatively, implement the Hill function within the C++ code generated. 
 @check_units(x=mmolar, e=mmolar, n=1, result=1)
 def Hill(x, e, n):
-	return x**n / (x**n + e**n)
+    return x**n / (x**n + e**n)
 Hill.stateless = True
 
 ### Astrocytes
 astro_eqs_evan_thesis = '''
 # WHAT IS RHO? CONSTANT THROUGHOUT?
-rho : 1
-vol_coef = Lambda*(1-rho) : meter**3
-rhoA = rho / (1-rho) : 1
+#################################
+#   Morphology
+################################
+# Lambda is the reference volume of the compartment, taken as the soma volume
+rho                       : 1  # Ratio of ER to Compartment vol
+rho_A = rho / (1-rho)     : 1
+vol_coef = Lambda*(1-rho) : meter**3  # Cytosolic volume
 
-dCaCYT/dt = Jchan + Jleak - Jpump : mmolar
-dCaER/dt  = -(Jchan + Jleak - Jpump) / rhoA : mmolar
-dh/dt = (h_inf - h) * Omega_h                                  : 1
+##############################
+#  Governing equations
+##############################
+dCaCYT/dt = Jchan + Jleak - Jpump            : mmolar
+dCaER/dt  = -(Jchan + Jleak - Jpump) / rho_A : mmolar
+dh/dt = 0*(h_inf - h) * Omega_h                : 1
+
 # Input J_ex or J_beta missing
-dI/dt = (J_ex + J_delta - J_3K - J_5P + J_coupling) / vol_coef : mmolar   # return to J_coupling once code works
+dI/dt = (J_ex + J_delta - J_3K - J_5P + J_coupling) / vol_coef : mmolar
 
+###############################
+#   Currents / Fluxes
+###############################
 
-#J_delta = O_delta * Hill(kappa_delta, I, 1) * Hill(CaCYT, K_delta, 2.): mole/second
-J_delta = 0*O_delta * (kappa_delta / (kappa_delta+I)) * (CaCYT**2 / (CaCYT**2+K_delta**2)) : mole/second
+J_delta = 1*O_delta * (kappa_delta / (kappa_delta+I)) * (CaCYT**2 / (CaCYT**2+K_delta**2)) : mole/second # I close to the same
 
-#J_3K = O_3K * Hill(k_d, CaCYT, 4) * Hill(I, k_3K, 1.)          : mole/second  
-J_3K = 0*O_3K * (k_d**4 / (CaCYT**4+k_d**4)) * (I / (I + k_3K))   : mole/second  
+J_3K = 0*O_3K * (CaCYT**4 / (CaCYT**4+k_d**4)) * (I / (I + k_3K))   : mole/second  # I not the same as MDP
 
 # Simplified version: J_5P = rbar * I (units of rbar are then Hz
-#J_5P = O_5P * Hill(I, k_5P, 1)                                    : mole/second
-J_5P = 0*O_5P * (I / (I + k_5P))      : mole/second
+J_5P = 0*O_5P * I / (I + k_5P)      : mole/second  # I exactly the same  as MDP
 
 Q_2 = d_2 * (I + d_1)/(I + d_3)                           : mmolar
 #minf = Hill(I, d_1, 1)   : 1
@@ -146,17 +165,17 @@ minf = I / (I + d_1) : 1
 
 #ninf = Hill(CaCYT, d_5, 1)  : 1
 ninf = CaCYT / (CaCYT + d_5) : 1
-Jchan = r_c * (minf*ninf*h)**3 * (CaER - CaCYT)     : mmolar/second
-Jleak = r_l * (CaER - CaCYT)                          : mmolar/second
 
-#Jpump = v_er * Hill(CaCYT, k_er, 2)                   : mmolar/second
-Jpump = v_er * CaCYT**2 / (CaCYT**2 + k_er**2)      : mmolar/second
+Jchan = 1*r_c * (minf*ninf*h)**3 * (CaER - CaCYT)     : mmolar/second
+#  J_leak =  1*Omega_L * (C_T - (1 + rho_A)*C)   ## MDP code
+Jleak = 1*r_l * (CaER - CaCYT)                          : mmolar/second
+Jpump = 1*v_er * CaCYT**2 / (CaCYT**2 + k_er**2)      : mmolar/second
 
-#h_inf = Hill(Q_2, CaCYT, 1)                           : 1
-h_inf = Q_2 / (Q_2 + CaCYT)                : 1
+#h_inf = Hill(Q_2, CaCYT, 1)                          : 1
+h_inf = Q_2 / (Q_2 + CaCYT)                           : 1
 
-#Omega_h = Omega_2 * (Q_2 + CaCYT)                     : Hz
-Omega_h = a_2 * (Q_2 + CaCYT)                        : Hz
+#Omega_h = Omega_2 * (Q_2 + CaCYT)                    : Hz
+Omega_h = a_2 * (Q_2 + CaCYT)                         : Hz
 #tau_h = 1/(a_2 * (Q_2 + CaCYT))                      : second  #(REVISIT CHOICES OF COEFFICIENTS)
 
 # Exogenous stimulation (rectangular wave with period of 50s and duty factor 0.4)
@@ -175,16 +194,19 @@ I_bias : mmolar (constant)
 
 N_astro = 2 # Total number of astrocytes in the network
 astrocytes = NeuronGroup(N_astro, astro_eqs_evan_thesis, method='rk4')
-astrocytes.I_bias = np.asarray([10, 0.],dtype=float)*umolar
-astro_mon = StateMonitor(astrocytes, variables=['CaER', 'CaCYT', 'I', 'J_coupling', 'vol_coef', 'J_ex'], record=True)
+astro_mon = StateMonitor(astrocytes, variables=['CaER', 'CaCYT', 'I', 'J_coupling', 'vol_coef', 
+   'J_delta', 'J_ex', 'Jchan', 'Jleak', 'Jpump', 'rho_A', 'h'], record=True)
 
+#######################################
+#   Initial conditions
+#######################################
 #astrocytes.CaCYT = [0.9,0.0]*umolar
 #astrocytes.CaER = [7.5,7.5]*umolar
 #astrocytes.h =  [0.95,0.95]
 #astrocytes.I = [0.100,0.2]*umolar  # umolar   # Units: umolar or ymolar? 
 astrocytes.rho = 0.18
 astrocytes.I_bias = np.asarray([10, 0.],dtype=float)*umolar
-astrocytes.CaER = 2*umolar   # C_T in MDP code
+astrocytes.CaER = 2.*umolar/rho_A   # C_T in MDP code
 
 ##########################################3
 # Diffusion between astrocytes
@@ -204,13 +226,17 @@ astro_to_astro.connect(True)
 ################################################################################
 run(duration, report='text')
 
-print(syn_mon.coef)
-print("astro_to_astro= ", syn_mon.coef)
-print("CaCYT: ", astro_mon.CaCYT)
-print("CaER: ", astro_mon.CaER)
-print("I: ", astro_mon.I)
-print("J_coupling: ", astro_mon.J_coupling)
-#print("vol_coef: ", astro_mon.vol_coef)
+print("CaCYT: ", astro_mon.CaCYT[0])
+print("CaER: ", astro_mon.CaER[0])
+print("I: ", astro_mon.I[0])
+print("Jchan= ", astro_mon.Jchan[0])
+print("Jleak= ", astro_mon.Jleak[0])
+print("Jpump= ", astro_mon.Jpump[0])
+print("rho_A= ", astro_mon.rho_A[0])
+print("J_coupling: ", astro_mon.J_coupling[0])
+print("vol_coef= ", astro_mon.vol_coef[0])
+print("h= ", astro_mon.h[0])
+print("J_delta= ", astro_mon.J_delta[0])
 
 ################################################################################
 # Analysis and plotting
@@ -233,6 +259,6 @@ ax = axes[3]
 ax.plot(astro_mon.t/second,astro_mon.J_ex[0]/umolar,'b-')
 ax.plot(astro_mon.t/second,astro_mon.J_ex[1]/umolar,'r-')
 ax.set_ylabel("J_ex")
-plt.tight_layout()
+#plt.tight_layout()
 
-plt.savefig("plot.pdf")
+plt.savefig("plot.pdf", bbox_inches='tight')
--- Code/gordon/two_compartments_MDP_2019-11-16.py
+++ Code/gordon/two_compartments_MDP_2019-11-16.py
@@ -17,7 +17,8 @@ rate_ = lambda x,L,r : x*L*(1-r)
 ################################################################################
 ### General parameters
 duration = 60*second       # Total simulation time
-sim_dt = 50*ms               # Integrator/sampling step
+duration = 150*ms          # Total simulation time
+sim_dt = 50*ms             # Integrator/sampling step
 
 ### Astrocyte parameters
 # ---  Calcium fluxes
@@ -29,6 +30,7 @@ O_P = 1.0*umolar/second      # Maximal Ca^2+ uptake rate by SERCAs  (0.9 in MDP,
 K_P = 0.05 * umolar          # Ca2+ affinity of SERCAs
 C_T = 2*umolar               # Total cell free Ca^2+ content
 rho_A = 0.18                 # ER-to-cytoplasm volume ratio
+rho = rho_A / (1.+rho_A)     # ER-to-cytoplasm volume ratio
 Omega_C = 6/second           # Maximal rate of Ca^2+ release by IP_3Rs
 Omega_L = 0.1/second         # Maximal rate of Ca^2+ leak from the ER
 # --- IP_3R kinectics
@@ -40,7 +42,7 @@ d_5 = 0.08*umolar            # Ca^2+ activation dissociation constant
 # --- IP_3 production
 O_delta = 0.6*umolar/second  # Maximal rate of IP_3 production by PLCdelta
 kappa_delta = 1.5* umolar    # Inhibition constant of PLC_delta by IP_3
-K_delta = 0.1*umolar         # Ca^2+ affinity of PLCdelta
+K_delta = 0.1*umolar         # Ca^2+ affinity of PLCdelta   <<<<<<<<<<<<<<<<<
 # --- IP_3 degradation
 O_5P = 0.05/second       # Maximal rate of IP_3 degradation by IP-5P
 K_5P = 10*umolar
@@ -55,13 +57,19 @@ omega_I = 0.05*umolar        # Scaling factor of diffusion
 ################################################################################
 # Additional and modified parameters
 ################################################################################
+# Volume of an average soma
 Lambda = 2100*umeter**3
 
-O_delta = 0.6 * umolar * Lambda * (1-rho_A) / second
-O_3K    = 4.5 * umolar * Lambda * (1-rho_A) / second
-O_5P    = 0.05 * umolar * Lambda * (1-rho_A) / second
+# Multiply value of amplitudes by cytosolic volume (changed rho_A to rho)
+O_delta = 0.6  * umolar * Lambda * (1-rho) / second
+O_3K    = 4.5  * umolar * Lambda * (1-rho) / second
+O_5P    = 0.05 * umolar * Lambda * (1-rho) / second
 F       = 0.1 / second
 D       = 0.05 / second  # setting D to zero has no effect. Why?
+
+print("**** Omega_L= ", Omega_L)
+print("**** O_delta= ", O_delta) # 0.6
+print("**** K_delta= ", K_delta) # 0.6
 
 ################################################################################
 # Model definition
@@ -72,30 +80,35 @@ defaultclock.dt = sim_dt     # Set the integration time
 astro_eqs = '''
 vol_coef = Lambda*(1-rho_A) : meter**3
 dI/dt = (J_ex + J_delta - J_3K - J_5P + J_coupling)/vol_coef : mmolar
-J_delta = 0*O_delta/(1 + I/kappa_delta) * C**2/(C**2 + K_delta**2) : mole/second
-J_3K = 0*O_3K * C**4/(C**4 + K_D**4) * I/(I + K_3K)                : mole/second
+J_delta = 1*O_delta/(1 + I/kappa_delta) * C**2/(C**2 + K_delta**2) : mole/second
+
+J_3K = 0*O_3K * C**4/(C**4 + K_D**4) * I/(I + K_3K)                : mole/second  # ERROR? 
+#J_3K = 1*O_3K * (k_d**4 / (CaCYT**4+k_d**4)) * (I / (I + k_3K))   : mole/second  # I not the same as MDP
+
 J_5P = 0*O_5P*I/(I + K_5P)                                         : mole/second
 
 # Exogenous stimulation (rectangular wave with period of 50s and duty factor 0.4)
-stimulus = int((t % (50*second))<20*second)                      : 1
-delta_I_bias = I - I_bias*stimulus                               : mmolar
+stimulus = int((t % (50*second))<20*second)                  : 1
+delta_I_bias = I - I_bias*stimulus                           : mmolar
 
 # external input (called J_beta in Evan's thesis)
-J_ex = 1*(-F*delta_I_bias*vol_coef)                                  : mole/second 
+J_ex = 1*(-F*delta_I_bias*vol_coef)                          : mole/second 
 
 # Diffusion between astrocytes
-J_coupling                                                       : mole/second
+J_coupling                                                   : mole/second
 
-# Ca^2+-induced Ca^2+ release:
-dC/dt = J_r + J_leak - J_pump                                : mmolar
-dh/dt = (h_inf - h)/tau_h                                 : 1
-J_r =  Omega_C * m_inf**3 * h**3 * (C_T - (1 + rho_A)*C) : mmolar/second
-J_leak =  Omega_L * (C_T - (1 + rho_A)*C)                     : mmolar/second
+# Ca^2+-induced Ca^2+ release:  (WHY IS CALCIUM ALWAYS ZERO? Currents Are not zero!!)
+dC/dt = (J_chan + J_leak - J_pump)                           : mmolar
+dh/dt = 0.*(h_inf - h)/tau_h                                 : 1
+
+J_chan =  Omega_C * m_inf**3 * h**3 * (C_T - (1 + rho_A)*C)   : mmolar/second
+J_leak =  1*Omega_L * (C_T - (1 + rho_A)*C)                     : mmolar/second
 J_pump =  O_P * C**2/(C**2 + K_P**2)                          : mmolar/second
-m_inf = I/(I + d_1) * C/(C + d_5)                         : 1
-h_inf = Q_2/(Q_2 + C)                                     : 1
-tau_h = 1/(O_2 * (Q_2 + C))                               : second
-Q_2 = d_2 * (I + d_1)/(I + d_3)                           : mmolar
+
+m_inf = I/(I + d_1) * C/(C + d_5)                            : 1
+h_inf = Q_2/(Q_2 + C)                                        : 1
+tau_h = 1/(O_2 * (Q_2 + C))                                  : second
+Q_2 = d_2 * (I + d_1)/(I + d_3)                              : mmolar
 
 # External IP_3 drive
 I_bias : mmolar (constant)
@@ -104,7 +117,8 @@ I_bias : mmolar (constant)
 N_astro = 2 # Total number of astrocytes in the network
 astrocytes = NeuronGroup(N_astro, astro_eqs, method='rk4')
 astrocytes.I_bias = np.asarray([10, 0.],dtype=float)*umolar
-astro_mon = StateMonitor(astrocytes, variables=['C', 'I', 'J_ex'], record=True)
+astro_mon = StateMonitor(astrocytes, variables=['C', 'I', 'J_ex', 'J_chan', 'J_leak', 'J_pump', 'J_delta', 
+    'vol_coef', 'J_coupling'], record=True)
 
 # Diffusion between astrocytes
 astro_to_astro_eqs = '''
@@ -122,8 +136,15 @@ astro_to_astro.connect(True)
 ################################################################################
 run(duration, report='text')
 
-print(syn_mon.coef)
-print("astro_to_astro= ", syn_mon.coef)
+#print("coef= ", syn_mon.coef)
+print("C= ", astro_mon.C[0])
+print("I= ", astro_mon.I[0])
+print("J_chan= ", astro_mon.J_chan[0])
+print("J_leak= ", astro_mon.J_leak[0])
+print("J_pump= ", astro_mon.J_pump[0])
+print("vol_coef= ", astro_mon.vol_coef[0])
+print("J_coupling= ", astro_mon.J_coupling[0])
+print("J_delta= ", astro_mon.J_delta[0])
 
 ################################################################################
 # Analysis and plotting
