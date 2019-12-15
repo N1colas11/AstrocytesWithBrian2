f8ff98943cae26e10524d0fe94836d4106470b00
Evan Cresswell <evancresswell@gmail.com>
2019-12-15 10:33:11 -0500

    swc file is important and rho is added

--- Code/ecc/ex_swc.py
+++ Code/ecc/ex_swc.py
@@ -23,15 +23,20 @@ import matplotlib.pylab as plt
 file_name = 'ex_astro.swc'
 morphology = swc.read_swc(file_name)
 
+
 # the compartment list has all of the nodes in the file
 # print soma compartment data
 if True:
     import pprint
-    pprint.pprint(morphology.compartment_list)
+    #pprint.pprint(morphology.compartment_list)
 
     fig, axes = plt.subplots(1, 2, sharey=True, sharex=True)
     # Make a line drawing of x-y and y-z views
-    for n in morphology.compartment_list:
+    for i,n in enumerate(morphology.compartment_list):
+        pprint.pprint(morphology.compartment_list[i])
+        morphology.compartment_list[i]['rho'] = .5
+        #print("compartment ", i," radius ", morphology.compartment_list[i]['radius'])
+        print("compartment ", i," rho ", morphology.compartment_list[i]['rho'])
         for c in morphology.children_of(n):
             axes[0].plot([n['x'], c['x']], [n['y'], c['y']], color='black')
             axes[1].plot([n['z'], c['z']], [n['y'], c['y']], color='black')
