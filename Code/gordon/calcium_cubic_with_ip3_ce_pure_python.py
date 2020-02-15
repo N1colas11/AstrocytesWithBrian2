Diffusion terms are dealt with. They might have the wrong sign.
Let to do: deal with the units.

Code/gordon/calcium_cubic_with_ip3_ce_pure_python.py
@@ -108,7 +108,7 @@ class Parameters:
         # Another complex calculation that depends on solution to a cubic equation
         # eqs. (73)-(76) in De Pitta's notes. MUST IMPLEMNENT. 
         # Later, evaluate the terms and figure out what is negligeable and what is not. 
-        self.dv_ER = 0  #* mvolt
+        self.dv_ER = 10  #* mvolt  (CANNOT BE zero, else a NaN will result)
     
         self.K_P = 0.05  #* umolar          # Ca2+ affinity of SERCAs
 
@@ -161,6 +161,7 @@ class RHS():
         # h : fraction of open channels
         # h : IP3 concentration
 
+        # rhs_C, etc. Are arrays, created via implicit vectorization involving numpy arrays
 
         dR2 = g.R**2 - g.r**2  #: meter**2
         s = g.R + g.r          #: meter
@@ -185,29 +186,23 @@ class RHS():
         Tot_CE = Ce               #: mole/meter**3  (sum of Ce in two half compartments in Brian2 code)
 
         Jr     = (2*g.r/dR2) * g.P_r * g.p_open * (Ce-C) #: mole/meter**3/second  # p_open, Pr
-        J1     = (4*g.P_CE/g.r*VVT) * g.dv_ER * (C*np.exp(-2.*g.dv_ER/VVT) - Ce) / (np.exp(-2.*g.dv_ER/VVT)-1.) #: mole/meter**3/second  # dv_ER
+        #J1     = (4*g.P_CE/g.r*VVT) * g.dv_ER * (C*np.exp(-2.*g.dv_ER/VVT) - Ce) / (np.exp(-2.*g.dv_ER/VVT)-1.) #: mole/meter**3/second  # dv_ER
+        print(g.dv_ER, ", VVT= ", VVT)
+        xxx = -2.*g.dv_ER/VVT
+        J1     = (4*g.P_CE/g.r*VVT) * g.dv_ER * (C*np.exp(-2.*g.dv_ER/VVT) - Ce) / (np.exp(-xxx)-1.) #: mole/meter**3/second  # dv_ER
+        print("J1= ",J1)
         Jp     = (2*g.r*g.d_ER)/(g.N_A*dR2) * g.Omega_u * g.eta_p * C**2 / (C**2 + g.K_P**2) #: mole/meter**3/second # d_ER, N_A, Omega_u, eta_p, K_p
+        print("Jp= ",Jp)
         #minf   =  I / (I + d_1)  : 1 # d_1
         #ninf   = C / (C + d_5) : 1 # d_5
         #hinf   =  d_2 * (I + d_1) / (d_2*(I +d_1) + (I+d_3)*C) : 1 # d_2, d_1, d_3
 
         # Must sum over two compartments. How to do this within this structure?
-        nb_connections = 1
-        A1 = (0.5 * g.F * s)/(g.Cm * g.V_T) * (dR2 / g.L)         #: meter**4 / mole (summed)
-        B1 = (1. - (s * g.F * C)/(2. * g.Cm * g.V_T))            #: meter (summed)
-        C1 = dR2 * C / g.L                              #: mole / meter**2 (summed)
-        CC0 = ((-B1 + np.sqrt(B1**2 + 4*C1*A1)) / (2.*A1)) / nb_connections #: mole / meter**3 (summed)
-        V0 = (g.Vrest + (CC0 - g.Crest) * (g.F * s) / g.Cm) / nb_connections   #: volt  (summed)
 
-        electro_diffusion = -g.P_Ca * V / (g.R * g.V_T) * (Ce*np.exp(-2*V/g.V_T) - Tot_C) / (np.exp(-2*V/g.V_T) - 1) #: mole / second / meter**3
-        coupling_electro = (4*g.D_C/g.L**2/g.V_T) * (CC0 + Tot_C) * (V0 - V) #: mole/second/meter**3 (summed) 
-        coupling_C = (4*g.D_C / g.L**2) * (Tot_C - Tot_C) #: mole/second/meter**3 (summed)  # ERROR. Cannot be zero.
-
-        rhs_C = 1*coupling_C + 1.*coupling_electro + 0.*electro_diffusion + Jr + J1 - Jp  #: mole / meter**3
+        rhs_C = Jr + J1 - Jp  #: mole / meter**3
 
         # Endoplasmic Reticulum Dynamnics
-        coupling_Ce = (4*g.D_CE / g.L**2) * (Tot_CE - Tot_CE) #: mole/second/meter**3 (summed)  (CHECK) (most be Tot_Ce from each compartment 
-        rhs_Ce = 1*coupling_Ce + Jp/(g.rho*g.Lambda) - (Jr + J1)  #: mole/meter**3
+        rhs_Ce = Jp/(g.rho*g.Lambda) - (Jr + J1)  #: mole/meter**3
 
         #IP3 dynamics
         Jbeta  = 0  #*mole/meter**3/second  : mole/meter**3/second
@@ -217,13 +212,40 @@ class RHS():
 
         # assume L constant (CHECK)
         Tot_I  = I                #: mole/meter**3  (sum of Ce in two half compartments in Brian2 code)
-        coupling_I  = (4*g.D_I  / g.L**2) * (Tot_I  - Tot_I)  # : mole/second/meter**3 (summed) (CHECK)
-        rhs_I = (Jbeta + Jdelta - J3K - J5P) / (g.Lambda*(1-g.rho)) + 1*coupling_I   #: mole/meter**3
+        coupling_I  = (4*g.D_I  / g.L**2) * (I  - I)  # : mole/second/meter**3 (summed) (CHECK)
+        rhs_I = (Jbeta + Jdelta - J3K - J5P) / (g.Lambda*(1-g.rho)) #: mole/meter**3
 
         # Open Channel dynamics
         OmegaH = (g.Omega_2*(I+g.d_1) + g.O_2*(I+g.d_3)*C) / (I + g.d_3) #: Hz # Omega_2, O_2, d_1, d_3
         hinf   =  g.d_2 * (I + g.d_1) / (g.d_2*(I + g.d_1) + (I + g.d_3)*C) #: 1 # d_2, d_1, d_3
         rhs_h = OmegaH * (hinf - h) #: 1
+
+		#-----------------------------
+		# Handle diffusion terms, which involve terms from two adjacent compartments
+        for i in [0,1]:
+            A1 = (0.5 * g.F * s)/(g.Cm * g.V_T) * (dR2 / g.L)         #: meter**4 / mole (summed)
+            B1 = (1. - (s * g.F * C[i])/(2. * g.Cm * g.V_T))            #: meter (summed)
+            C1 = dR2 * C[i] / g.L                              #: mole / meter**2 (summed)
+
+        nb_connections = 1
+        CC0 = ((-B1 + np.sqrt(B1**2 + 4*C1*A1)) / (2.*A1)) / nb_connections #: mole / meter**3 (summed)
+        V0 = (g.Vrest + (CC0 - g.Crest) * (g.F * s) / g.Cm) / nb_connections   #: volt  (summed)
+
+        coupling_Ce = (4*g.D_CE / g.L**2) * (Ce[0]-Ce[1]) * np.array([1,-1])  #: mole/second/meter**3 (summed)  (CHECK) (most be Tot_Ce from each compartment 
+        electro_diffusion = -g.P_Ca * V / (g.R * g.V_T) * (Ce*np.exp(-2*V/g.V_T) - C) / (np.exp(-2*V/g.V_T) - 1) #: mole / second / meter**3
+        coupling_electro = (4*g.D_C/g.L**2/g.V_T) * (CC0 + C) * (V0 - V)  #: mole/second/meter**3 (summed) 
+        coupling_C = (4*g.D_C / g.L**2) * (C[0] - C[1]) * np.array([1,-1]) #: mole/second/meter**3 (summed)  # ERROR. Cannot be zero.
+
+        # All coupling terms
+        rhs_C  += 1*coupling_C + 1.*coupling_electro + 0.*electro_diffusion #: mole / meter**3
+        rhs_Ce += 1*coupling_Ce
+        rhs_I  += 1*coupling_I   #: mole/meter**3
+
+        print(rhs_C)
+        print(rhs_Ce)
+        print(rhs_I)
+        print(rhs_h)
+
 #----------------------------------------------------------------------
 g = Parameters()
 
@@ -232,328 +254,9 @@ rhs1 = RHS(g)
 rhs2 = RHS(g)  # must clean this up. Probably compartment should only store state variables. 
                # the RHS equations are really the same for all compartments, except diffusion terms. 
                # not yet clear how to handle these. 
-rhs1.rhs(1.,1.,1.,1.)  # arbitrary values of C, Ce, I, h
+C  = np.array([1,1])
+Ce = np.array([1,1])
+h  = np.array([1,1])
+I  = np.array([1,1])
+rhs1.rhs(C, Ce, h, I)  # arbitrary values of C, Ce, I, h
 #----------------------------------------------------------------------
-"""
-# has large impact on diffusion. Creates instability
-coupling_electro   : mole / second / meter**3
-#coupling_electro = 1  : 1 
-
-# Tot_C is the total calcium (computed in synapses2)
-Tot_C : mole/meter**3
-
-V = Vrest + (F*s/Cm) * (Tot_C-Crest) : volt
-VVT = -2.*V/V_T : 1  # value of 0, which leads to a zero denominator
-
-# Has minimal impact on Calcium
-electro_diffusion = -P_Ca * V / (R * V_T) * (Ce*exp(-2*V/V_T) - Tot_C) / (exp(-2*V/V_T) - 1) : mole / second / meter**3
-
-A1 : meter**4 / mole 
-B1 : meter 
-C1 : mole / meter**2 
-
-CC0 : mole / meter**3
-V0 : volt
-nb_connections : 1  # number of synaptic connections
-
-# Calcium diffusion
-# Each term will handle half a compartment 
-
-dC/dt = 1*coupling_C + 1.*coupling_electro + 0.*electro_diffusion + Jr + J1 - Jp  : mole / meter**3
-
-####  ENDOPLASMIC RETICULUM
-Tot_CE                 : mole/meter**3
-coupling_CE            : mole/second/meter**3
-dCE/dt = 1*coupling_CE + Jp/(rho*Lambda) - (Jr + J1)  : mole/meter**3
-
-####  IP3
-Tot_I                  : mole/meter**3
-coupling_I             : mole/second/meter**3
-dI/dt = (Jbeta + Jdelta - J3K - J5P) / (Lambda*(1-rho)) + 1*coupling_CE   : mole/meter**3
-
-### Open Channels
-dh/dt = OmegaH * (hinf - h) : 1
-
-
-################3
-###  CURRENTS
-
-Jr     = (2*r/dR2) * P_r * p_open * (CE-C) : mole/meter**3/second  # p_open, Pr
-J1     = (4*P_CE/r*VVT) * dv_ER * (C*exp(-2.*dv_ER/VVT) - CE) / (exp(-2.*dv_ER/VVT)-1.) : mole/meter**3/second  # dv_ER
-Jp     = (2*r*d_ER)/(N_A*dR2) * Omega_u * eta_p * C**2 / (C**2 + K_P**2) : mole/meter**3/second # d_ER, N_A, Omega_u, eta_p, K_p
-minf   =  I / (I + d_1)  : 1 # d_1
-ninf   = C / (C + d_5) : 1 # d_5
-hinf   =  d_2 * (I + d_1) / (d_2*(I +d_1) + (I+d_3)*C) : 1 # d_2, d_1, d_3
-OmegaH = (Omega_2*(I+d_1) + O_2*(I+d_3)*C) / (I + d_3) : Hz # Omega_2, O_2, d_1, d_3
-
-
-
-#IP3 dynamics
-Jbeta  = 0*mole/meter**3/second  : mole/meter**3/second
-Jdelta = o_delta * (k_delta/(I+k_delta)) * (C**2/(C**2+K_delta**2)) : mole/second  # not sure about units, o_delta, k_delta, K_delta
-J5P    = o_5P * (I/(I+K_5P)) : mole/second # o_5P, K_5P
-J3K    = o_3K * (C**4/(C**4+K_D**4)) * (I/(I+K_3)) : mole/second # o_3K, K_D, K_3
-
-'''
-
-N_astro = 3 # Total number of astrocytes1 in the network (each compartment broken into two
-N_astro = 2*N_astro # Total number of astrocytes1 in the network (each compartment broken into two
-astrocytes1 = NeuronGroup(N_astro, astro_eqs, method='euler', name='ng1', order=1)
-
-# Initital Conditions
-astrocytes1.L = 8 *  umeter  # length of a compartment
-astrocytes1.R = 3 * umeter
-astrocytes1.r = 2 * umeter
-# Calcium in 1/2 compartment should be the value for the full compartment since the 1/2 compartment
-# Each 1/2 compartment only considers the fork at one end. 
-# is an artifact of the numerical scheme.
-astrocytes1.C = 1.1e-4  * mole / meter**3
-astrocytes1.I = 1.1e-4  * mole / meter**3
-astrocytes1.CE = 1.1e-4  * mole / meter**3
-#astrocytes1.C = [1.1e-4, 1.5e-4, 1.6e-4]*2 * mole / meter**3
-
-astro_mon = StateMonitor(astrocytes1, variables=['Tot_C', 'Tot_CE', 'Tot_I', 'coupling_C', 'coupling_electro', 'electro_diffusion', 'nb_connections', 'A1', 'B1', 'C1', 'CC0', 'V0'], record=True)
-b_mon = StateMonitor(astrocytes1, variables=['CC0', 'dR2', 's', 'L'], record=True)
-
-'''
-One issue is Boundary Conditions. I have not done anything to implement those. 
-Also, each synapse has its own value of V0. 
-'''
-
-#----------------------------------------------------------------------
-# Diffusion between astrocytes1
-synapse1_eqs = '''
-# Assuming the above is correct, let us figure out units. 
-# using C_pre:  C * F * s / Cm = V  ==> F / Cm = V / s / C = (V / s) * m^3 / mole
-
-####  CYTOPLASM
-
-# A1 stable: value does not change from iteration to iteration
-A1_pre = (0.5 * F * s_post)/(Cm * V_T) * (dR2_post / L_post) : meter**4 / mole (summed)
-B1_pre = (1. - (s_post * F * Tot_C_post)/(2. * Cm * V_T)) : meter (summed)
-C1_pre = dR2_post * Tot_C_post / L_post : mole / meter**2 (summed)
-
-# Only computed to trick Brian into computing C0_pre by summing the values for all synapses (which will 
-#   all bequal  since it depends on A1, B1, C1, for which the summation has already been executed. 
-"""
-#----------------------------------------------------------------------
-
-
-#----------------------------------------------------------------------
-astro_eqs = '''
-s = R + r          : meter
-r                  : meter
-dR2 = R**2 - r**2  : meter**2
-DR2                : meter**2
-L                  : meter
-R                  : meter  # Outer radius
-coupling_C         : mole/second/meter**3
-
-####  CALCIUM
-
-# has large impact on diffusion. Creates instability
-coupling_electro   : mole / second / meter**3
-#coupling_electro = 1  : 1 
-
-# Tot_C is the total calcium (computed in synapses2)
-Tot_C : mole/meter**3
-
-V = Vrest + (F*s/Cm) * (Tot_C-Crest) : volt
-VVT = -2.*V/V_T : 1  # value of 0, which leads to a zero denominator
-
-# Has minimal impact on Calcium
-electro_diffusion = -P_Ca * V / (R * V_T) * (Ce*exp(-2*V/V_T) - Tot_C) / (exp(-2*V/V_T) - 1) : mole / second / meter**3
-
-A1 : meter**4 / mole 
-B1 : meter 
-C1 : mole / meter**2 
-
-CC0 : mole / meter**3
-V0 : volt
-nb_connections : 1  # number of synaptic connections
-
-# Calcium diffusion
-# Each term will handle half a compartment 
-
-dC/dt = 1*coupling_C + 1.*coupling_electro + 0.*electro_diffusion + Jr + J1 - Jp  : mole / meter**3
-
-####  ENDOPLASMIC RETICULUM
-Tot_CE                 : mole/meter**3
-coupling_CE            : mole/second/meter**3
-dCE/dt = 1*coupling_CE + Jp/(rho*Lambda) - (Jr + J1)  : mole/meter**3
-
-####  IP3
-Tot_I                  : mole/meter**3
-coupling_I             : mole/second/meter**3
-dI/dt = (Jbeta + Jdelta - J3K - J5P) / (Lambda*(1-rho)) + 1*coupling_CE   : mole/meter**3
-
-### Open Channels
-dh/dt = OmegaH * (hinf - h) : 1
-
-
-################3
-###  CURRENTS
-
-Jr     = (2*r/dR2) * P_r * p_open * (CE-C) : mole/meter**3/second  # p_open, Pr
-J1     = (4*P_CE/r*VVT) * dv_ER * (C*exp(-2.*dv_ER/VVT) - CE) / (exp(-2.*dv_ER/VVT)-1.) : mole/meter**3/second  # dv_ER
-Jp     = (2*r*d_ER)/(N_A*dR2) * Omega_u * eta_p * C**2 / (C**2 + K_P**2) : mole/meter**3/second # d_ER, N_A, Omega_u, eta_p, K_p
-minf   =  I / (I + d_1)  : 1 # d_1
-ninf   = C / (C + d_5) : 1 # d_5
-hinf   =  d_2 * (I + d_1) / (d_2*(I +d_1) + (I+d_3)*C) : 1 # d_2, d_1, d_3
-OmegaH = (Omega_2*(I+d_1) + O_2*(I+d_3)*C) / (I + d_3) : Hz # Omega_2, O_2, d_1, d_3
-
-
-
-#IP3 dynamics
-Jbeta  = 0*mole/meter**3/second  : mole/meter**3/second
-Jdelta = o_delta * (k_delta/(I+k_delta)) * (C**2/(C**2+K_delta**2)) : mole/second  # not sure about units, o_delta, k_delta, K_delta
-J5P    = o_5P * (I/(I+K_5P)) : mole/second # o_5P, K_5P
-J3K    = o_3K * (C**4/(C**4+K_D**4)) * (I/(I+K_3)) : mole/second # o_3K, K_D, K_3
-
-'''
-
-N_astro = 3 # Total number of astrocytes1 in the network (each compartment broken into two
-N_astro = 2*N_astro # Total number of astrocytes1 in the network (each compartment broken into two
-astrocytes1 = NeuronGroup(N_astro, astro_eqs, method='euler', name='ng1', order=1)
-
-# Initital Conditions
-astrocytes1.L = 8 *  umeter  # length of a compartment
-astrocytes1.R = 3 * umeter
-astrocytes1.r = 2 * umeter
-# Calcium in 1/2 compartment should be the value for the full compartment since the 1/2 compartment
-# Each 1/2 compartment only considers the fork at one end. 
-# is an artifact of the numerical scheme.
-astrocytes1.C = 1.1e-4  * mole / meter**3
-astrocytes1.I = 1.1e-4  * mole / meter**3
-astrocytes1.CE = 1.1e-4  * mole / meter**3
-#astrocytes1.C = [1.1e-4, 1.5e-4, 1.6e-4]*2 * mole / meter**3
-
-astro_mon = StateMonitor(astrocytes1, variables=['Tot_C', 'Tot_CE', 'Tot_I', 'coupling_C', 'coupling_electro', 'electro_diffusion', 'nb_connections', 'A1', 'B1', 'C1', 'CC0', 'V0'], record=True)
-b_mon = StateMonitor(astrocytes1, variables=['CC0', 'dR2', 's', 'L'], record=True)
-
-'''
-One issue is Boundary Conditions. I have not done anything to implement those. 
-Also, each synapse has its own value of V0. 
-'''
-
-#----------------------------------------------------------------------
-# Diffusion between astrocytes1
-synapse1_eqs = '''
-# Assuming the above is correct, let us figure out units. 
-# using C_pre:  C * F * s / Cm = V  ==> F / Cm = V / s / C = (V / s) * m^3 / mole
-
-####  CYTOPLASM
-
-# A1 stable: value does not change from iteration to iteration
-A1_pre = (0.5 * F * s_post)/(Cm * V_T) * (dR2_post / L_post) : meter**4 / mole (summed)
-B1_pre = (1. - (s_post * F * Tot_C_post)/(2. * Cm * V_T)) : meter (summed)
-C1_pre = dR2_post * Tot_C_post / L_post : mole / meter**2 (summed)
-
-# Only computed to trick Brian into computing C0_pre by summing the values for all synapses (which will 
-#   all bequal  since it depends on A1, B1, C1, for which the summation has already been executed. 
-# There must be an easier method without calculating C0 and V0 in the NeuronGroup
-nb_connections_pre = 1 : 1 (summed)
-
-# Notice I am dividing by the number of connections (3 if a single fork)
-CC0_pre = ((-B1_pre + sqrt(B1_pre**2 + 4*C1_pre*A1_pre)) / (2.*A1_pre)) / nb_connections_pre : mole / meter**3 (summed)
-V0_pre = (Vrest + (CC0_pre - Crest) * (F * s) / Cm) / nb_connections_pre   : volt  (summed)
-
-# Investigate why this should be a minus sign
-coupling_C_post = (4*D_C / L_post**2) * (Tot_C_pre - Tot_C_post) : mole/second/meter**3 (summed)
-
-# MAKE SURE CC0 and V0 are computed before updating the coupling parameters. 
-coupling_electro_pre = (4*D_C/L_post**2/V_T) * (CC0_post + Tot_C_post) * (V0_post - V_post) : mole/second/meter**3 (summed) 
-
-####  ENDOPLASMIC RETICULUM
-coupling_CE_post = (4*D_CE / L_post**2) * (Tot_CE_pre - Tot_CE_post) : mole/second/meter**3 (summed)
-
-####  IP3
-coupling_I_post = (4*D_I / L_post**2) * (Tot_I_pre - Tot_I_post) : mole/second/meter**3 (summed)
-'''
-#----------------------------------------------------------------------
-
-# TEMPORARY
-synapse2_eqs = '''
-    # C is updated in each compartment of the astrocyte and combined here
-    Tot_C_syn   = C_pre  + C_post    : mole / meter**3
-    Tot_C_pre   = Tot_C_syn       : mole / meter**3 (summed)
-
-    # update Calcium in compartments
-    # This works because synapses are bidirectional i --> j and j --> i
-
-####  ENDOPLASMIC RETICULUM
-    Tot_CE_pre  = Tot_CE_syn      : mole / meter**3 (summed)
-    Tot_CE_syn  = CE_pre + CE_post   : mole / meter**3
-
-####  IP3
-    Tot_I_syn = I_pre + I_post : mole / meter**3
-    Tot_I_pre = Tot_I_syn      : mole / meter**3 (summed)
-'''
-
-synapses1 = Synapses(astrocytes1, astrocytes1, model=synapse1_eqs, method='euler', order=0, name='sg1')
-synapses2 = Synapses(astrocytes1, astrocytes1, model=synapse2_eqs, method='euler', order=5, name='sg2')
-
-# Connections count from 0
-#  ---*---.---*----
-#         |___*____
-
-# Single fork (N_astro = 6)
-# Six Compartments: 0,1,...,4,5
-for i in range(N_astro):
-    synapses1.connect(i=i,j=i)
-pairs = [(3,1),(3,2),(1,2)]
-for pair in pairs:
-    synapses1.connect(i=pair[0], j=pair[1])
-    synapses1.connect(i=pair[1], j=pair[0])
-
-pairs = [(0,1),(2,4),(3,5)]
-for pair in pairs:
-    synapses2.connect(i=pair[0], j=pair[1])
-    synapses2.connect(i=pair[1], j=pair[0])
-
-s = synapses1.summed_updaters
-s['nb_connections_pre'].order = 0
-s['A1_pre'].order = 2
-s['B1_pre'].order = 2
-s['C1_pre'].order = 2
-s['CC0_pre'].order = 4
-s['V0_pre'].order = 5
-s['coupling_C_post'].order = 6
-s['coupling_electro_pre'].order = 7
-
-astrocytes1.state_updater.order = 10
-
-s = synapses2.summed_updaters
-s['Tot_C_pre'].order = 13
-
-
-for k,v in s.items():
-    print("updater[%s]= " % k, v)
-
-
-# Must call StateMonitor AFTER synaptic connections are established
-syn1_mon = StateMonitor(synapses1, variables=[], record=True, name='syn1_mon')
-syn2_mon = StateMonitor(synapses2, variables=[], record=True, name='syn2_mon')
-
-groups = [synapses1, synapses2]  #  must have i,j connections
-u.connectionMatrices(groups)
-
-
-#-------------------------------------------
-# Initial Conditions (call after connect())
-# Temporary. Should be computed in the synapse
-#synapses1.P                  = 1.1e-2 
-
-groups = [astrocytes1, synapses1, synapses2]
-u.lowLevelInfo(groups)
-
-# Run Simulation
-print(scheduling_summary())
-run(duration)
-#run(duration, report='text')
-
-u.printData(astro_mon, b_mon, syn1_mon, syn2_mon)
-quit()
-#u.plots(astro_mon)
-#----------------------------------------------------------------------
-