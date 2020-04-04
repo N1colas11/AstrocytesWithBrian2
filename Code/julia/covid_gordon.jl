37bb202582ee194bb645b57e7afe68a9aa3c604e
erlebach <gordon.erlebach@gmail.com>
2020-04-04 16:13:32 -0400

    Code now seems to run correctly.
    I would like to recreate the parameter study from the paper by
    varying boo, buu, buo (beta_uu, etc.) from some initial to some final
    value, controlled by the parameter bo.mult.init to bo.mult.revertR (R0)
    found in the author's R code.

--- Code/julia/covid_gordon.jl
+++ Code/julia/covid_gordon.jl
@@ -1,21 +1,11 @@
-using DifferentialEquations, Plots, BenchmarkTools
-using Profile, ProfileView, TimerOutputs
-using DataFrames
-#include("GlobalParameters.jl")
-#import .globalParameters_mod: globalParametersCorrect, globalParametersTest
-#include("global_functions.jl")
-#const GF = globalFunctions_mod
-#const GP = globalParameters_mod
-#const getParams = GP.globalParametersEvan
+using DifferentialEquations, Plots, BenchmarkTools,
+     Profile, ProfileView, TimerOutputs,
+     DataFrames
 
-function original_model()
-	# Taken from R code. This is what Nicolas did, but I want my own interpretation
-
-
-end
 
 function globalParameters()
   d = Dict{Symbol,Float64}()
+  d[:mult_init] =
   d[:base_mortality_ratio] = 1 / 30.
   d[:age_mortality_ratio]  = 1 / 50.
 
@@ -37,11 +27,9 @@ function globalParameters()
   d[:ρo]           = 0.15   # fraction of population over 65
   d[:Io]           = 1000   # number of infected individuals
   d[:Iy]           = 1000
-  d[:αu]           = .1
-  d[:αo]           = .1
   d[:βuu]          = .1     # under 65 / under 65 infection rate
-  d[:γ0]           = .1     # over 65 recovery rate
-  d[:δ0]           = .1     # over 65 mortality rate
+  d[:γ0]           = .1     # over 65 recovery rate (assumed constant)
+  d[:δ0]           = .1     # over 65 mortality rate (assumed constant)
   d[:Sf]           = 1.0    # fraction of total population suceptible to infection
   return d
 end
@@ -50,6 +38,10 @@ function derivedParameters(d)
 	d[:ρu]  = 1. - d[:ρo]  # fraction of population under 65
 	d[:γu]  = d[:γo] = d[:γ0]
 	d[:δu]  = d[:δo] = d[:δ0]
+
+	d[:αo] = d[:γo] + d[:δo]
+	d[:αu] = d[:γu] + d[:δu]
+
 	d[:β]   = d[:βuo] = d[:βou] = d[:βoo] = d[:βuu]     # under 65 / over 65 infection rate
 	#d[:αu]   = d[:γ0] * d[:ρ0] # Recovery rate + Death rate for old people
 	#ay=gy+Dy  #Recovery rate + Death rate for young people
@@ -58,33 +50,41 @@ function derivedParameters(d)
     d[:No0] = d[:N] * d[:ρo]  # initial population over 65   (called No0 and Ny0)
     d[:Nu0] = d[:N] * d[:ρu]  # initial population under 65
 
-	# Initial Conditions. But I will call the initial condition function later,
-	# which will use these paramters
+	d[:βoo] = d[:mult_init] * (d[:αo]*d[:ρo] +  d[:αu]*d[:ρu]) / d[:Sf]
+	d[:βuu] = d[:mult_init] * (d[:αu]*d[:ρu] +  d[:αu]*d[:ρu]) / d[:Sf]
+	d[:βou] = d[:βuo] = min(d[:βuu], d[:βoo])
+
+	#Initial Conditions. But I will call the initial condition function later,
+	#which will use these paramters
 	d[:Mo0] = 0                  # number of dead at t=0
 	d[:Mu0] = 0
 	d[:So0] = d[:No0] * d[:Sf]   # Total number of susceptibles at t=0
 	d[:Su0] = d[:Nu0] * d[:Sf]
 	d[:Ro0] = d[:No0] - d[:So0]  # Total number of recovered at =t0
 	d[:Ru0] = d[:Nu0] - d[:Su0]
-	# Initial fraction recovered population
-	d[:Io0] = d[:No0] - d[:So0] - d[:Ro0] - d[:Mo0]
-	d[:Iu0] = d[:Nu0] - d[:Su0] - d[:Ru0] - d[:Mu0]
-	println("sum (I+S+R+M) old+young: ",
-	   d[:Io0]+d[:Iu0]+d[:Ro0]+d[:Ru0]+d[:So0]+d[:Su0]+d[:Mo0]+d[:Mu0])
-	println("gordon: ",
-	    "Frances")
+	#Initial fraction recovered population
+	#d[:Io0] = d[:No0] - d[:So0] - d[:Ro0] - d[:Mo0]
+	#d[:Iu0] = d[:Nu0] - d[:Su0] - d[:Ru0] - d[:Mu0]
+	d[:Io0] = 1000
+	d[:Iu0] = 1000
+	# Make sure that the number of infected + number susceptible = total population at t=0, d[:N]
+	d[:So0] -= d[:Io0]
+	d[:Su0] -= d[:Iu0]
+	#println("globalParameters, sum (I+S+R+M) old+young: ",
+	#d[:Io0]+d[:Iu0]+d[:Ro0]+d[:Ru0]+d[:So0]+d[:Su0]+d[:Mo0]+d[:Mu0])
 end
 
 function initialConditions(p::Dict{Symbol,Float64})
-	# args p are parameters
-     I0 = p[:ρo]
+	 # args p are parameters
+     I0 = p[:Io0]
      S0 = p[:ρo] * p[:N]
-     R0 = .1
-     M0 = .1
-     i0 = p[:ρu]
+     R0 = p[:Ro0]
+     M0 = p[:Mo0]
+
+	 i0 = p[:Iu0]
      s0 = p[:ρu] * p[:N]
-     r0 = .1
-     m0 = .1
+     r0 = p[:Ru0]
+     m0 = p[:Mu0]
      uinit = vcat(I0,S0,R0,M0,i0,s0,r0,m0)
      return uinit
 end
@@ -92,60 +92,58 @@ end
 
 function Covid!(du, u, p, t)
     #@timeit to "Covid!" begin # Must be at least one space or won't work
-    I  = @view(u[1])[1]  #
-    S  = @view(u[2])[1]  #
-    R  = @view(u[3])[1]  #
-    M  = @view(u[4])[1]  #
-    i  = @view(u[5])[1]  #
-    s  = @view(u[6])[1]  #
-    r  = @view(u[7])[1]  #
-    m  = @view(u[8])[1]  #
-    #println("I,S,i,s= ", I,S,i,s)
-    #println("I,S,i,s= $I, $S, $i, $s")
+	I,S,R,M,i,s,r,m = u
+    du[1] =  (p[:βuo]*S*i + p[:βoo]*S*I)/p[:N] - p[:αo]*I   # I
+    du[2] = -(p[:βuo]*S*i + p[:βoo]*S*I)/p[:N]              # S
+    du[3] = +p[:γo]*I                                           # R
+    du[4] = +p[:δo]*I                                           # M
+    du[5] =  (p[:βuu]*s*i + p[:βuo]*s*I)/p[:N] - p[:αu]*I   # i
+    du[6] = -(p[:βuu]*s*i + p[:βuo]*s*I)/p[:N]              # s
+    du[7] = +p[:γu]*I                                           # r
+    du[8] = +p[:δu]*I                                           # m
+    #du[8] = p[:N] - sum(du[2:8])
+    old   = I+S+R+M
+    young = i+s+r+m
+    println("young,old: ", young/p[:N], "  , ", old/p[:N], "  , ", p[:N])
 
-	  αu = p[:γu] + p[:δu]
-	  αo = p[:γo] + p[:δo]
-
-	  du[1] =  p[:βuo]*S*i/p[:N] + p[:βoo]*S*I/p[:N] - p[:αo]*I   # I
-	  du[2] = -p[:βuo]*S*i/p[:N] - p[:βoo]*S*I/p[:N]              # S
-	  du[3] = +p[:γo]*I                                           # R
-	  du[4] = +p[:δo]*I                                           # M
-	  du[5] =  p[:βuu]*s*i/p[:N] + p[:βuo]*s*I/p[:N] - p[:αu]*I   # i
-	  du[6] = -p[:βuu]*s*i/p[:N] - p[:βuo]*s*I/p[:N]              # s
-	  du[7] = +p[:γu]*I                                           # r
-	  du[8] = +p[:δu]*I                                           # m
-	  old   = I+S+R+M
-	  young = i+s+r+m
-	  println("young+old: ", young+old)
-
-# d/dt [I + S + R + M] = (-p[:αo]*I + p[:γo] + p[:δo]) * I = 0
-# d/dt [i + s + r + m] = (-p[:αu]*I + p[:γu] + p[:δu]) * I = 0
-#
-# Old people stay old; young people stay young, so the balance
-#   above holds for each class individually.
-# Therefore, αo = γo + δo
-#    and     αu = γu + δu
-#
-#    I + S + R + M = const = p[:ρo]
-#    i + s + r + m = const = p[:ρu]
-
+      #d/dt [i + s + r + m] = (-p[:αu]*I + p[:γu] + p[:δu]) * I = 0
+      #d/dt [I + S + R + M] = (-p[:αo]*I + p[:γo] + p[:δo]) * I = 0
+      #.
+      #Old people stay old; young people stay young, so the balance
+      #.   above holds for each class individually.
+      #Therefore, αo = γo + δo
+      #.   and     αu = γu + δu
+      #.
+      #.    I + S + R + M = const = p[:ρo]
+      #.    i + s + r + m = const = p[:ρu]
 end
 
 pp = globalParameters()
 derivedParameters(pp)
+pp[:βuu] *= 10
 u0 = initialConditions(pp)
-tspan = (0., 365.)
-#tspan = (0., 5.)
+tspan = (0., 355.)
 
-
-prob = ODEProblem(Covid!, u0, tspan, pp)  # d is a parameter dictionary
-@time sol = solve(prob, Tsit5())
-#@time sol = solve(prob, Euler(), dt=.1)
-labels=[:I,:S,:R,:M,:i,:s,:r,:m]
-p = plot()
-for (i,label) ∈ enumerate(labels)
-	#print(i)
-	plot!(p, sol.t, sol[i,:], label=label)
+function solveProblem(tspan, u0, pp)
+	prob = ODEProblem(Covid!, u0, tspan, pp)  # d is a parameter dictionary
+	@time sol = solve(prob, Tsit5())
+	ss = @. sol[:,:] / pp[:N]
+	ss[1:4,:]
+	t1 = sum(ss[1:4,:], dims=2)
+	t2 = sum(ss[5:8,:], dims=2)
+	s = sum(ss, dims=1)
+	return sol
 end
-#print(sol)
-p
+
+function plotSolution(sol)
+	labels=[:I,:S,:R,:M,:i,:s,:r,:m]
+	p = plot()
+	for (i,label) ∈ enumerate(labels)
+		#print(i)
+		plot!(p, sol.t, sol[i,:], label=label)
+	end
+	p
+end
+
+sol = solveProblem(tspan, u0, pp)
+p = plotSolution(sol)
